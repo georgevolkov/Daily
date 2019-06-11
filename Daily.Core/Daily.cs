d2f522b92042cd16fcb2fa248151490d3d67eec9
George <GeorgeVolkov@gmail.com>
2019-06-11 16:46:41 +0600

    Add structure for DAL and core

---
+++ Daily.Core/Daily.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Daily.Core
+{
+    public class Daily
+    {
+        public string Before { get; set; }
+        public string After { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Daily.Core/Users.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography;
+
+namespace Daily.Core
+{
+    public class Users
+    {
+        public long UserId { get; set; }
+        public string UserName { get; set; }
+        public MD5 UserPassword { get; set; }
+    }
+}
